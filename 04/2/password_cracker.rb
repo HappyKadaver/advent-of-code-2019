3f0c171a0bbed22714c0c4ae862f93452f003616
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-12-08 14:45:04 +0100

    add day 4 part 2

---
+++ 04/2/password_cracker.rb
@@ -0,0 +1,15 @@
+def password_valid?(password)
+  digits = password.digits
+
+  digits.reverse.each_cons(2).to_a.all?{ |pair| pair.first <= pair.last } &&
+      digits.group_by(&:itself).values.map(&:count).any? {|count| count == 2}
+end
+
+def get_passwords(range_start, range_end)
+  (range_start..range_end).select(&method(:password_valid?))
+end
+
+start, range_end = IO.read('../input.txt').split('-').map &:to_i
+
+passwords = get_passwords(start, range_end)
+puts passwords.count
